f1d68ebe622081ab1ca4edcdb92231b688f9cdf8
VazDia <121106960+VazDia@users.noreply.github.com>
2022-12-27 20:35:23 +0000

    Exercice2_Daily_Challenge

---
+++ Exercice2.js
@@ -0,0 +1,9 @@
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+/*
+ En utilisant ce tableau
+ Accédez puis console.log « Oranges ».
+
+*/
+
+console.log(moreFruits[1][1][0]);
+
